cd82c3963b8d964018a372b7cf80cad4649464db
Samuel Demers <samuel@samdemers.com>
2014-02-23 23:17:01 -0500

    Improved kernel benchmark to run one thread per online CPU.

--- kernel-benchmark/lttng_benchmark.c
+++ kernel-benchmark/lttng_benchmark.c
@@ -1,7 +1,9 @@
+#include <linux/completion.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>
 #include <linux/module.h>
+#include <linux/slab.h>			
 
 #define CREATE_TRACE_POINTS
 #define TRACE_INCLUDE_PATH .
@@ -15,13 +17,22 @@ MODULE_PARM_DESC(default_count, "# of events generated");
 
 static unsigned int event_count;
 
-static ssize_t time_show(struct kobject *kobj, struct kobj_attribute *attr,
-			 char *buf)
+struct benchmark_thread {
+	struct task_struct *thread;
+	struct completion *prev;
+	struct completion end;
+	unsigned long long time;
+};
+
+static int benchmark_thread(void *arg)
 {
+	struct benchmark_thread *thread = (struct benchmark_thread *)arg;
 	struct timeval start_tv;
 	struct timeval end_tv;
 	unsigned long long time;
 	int i;
+
+	wait_for_completion_interruptible(thread->prev);
 
 	do_gettimeofday(&start_tv);
 	for (i = 0; i < event_count; i++)
@@ -32,8 +43,60 @@ static ssize_t time_show(struct kobject *kobj, struct kobj_attribute *attr,
 	time *= USEC_PER_SEC;
 	time += (long long)((long)end_tv.tv_usec - (long)start_tv.tv_usec);
 
-	printk(KERN_INFO "Generated %d events in %llu us\n", event_count, time);
+	thread->time = time;
 
+	complete(&thread->end);
+
+	return 0;
+}
+
+static ssize_t time_show(struct kobject *kobj, struct kobj_attribute *attr,
+			 char *buf)
+{
+	DECLARE_COMPLETION_ONSTACK(main_start);
+	struct benchmark_thread *threads;
+	unsigned int cpu;
+	unsigned int online_cpus = 0;
+	unsigned long long time = 0;
+
+	for_each_online_cpu(cpu) {
+		online_cpus++;
+	}
+
+	printk("lttng_benchmark: starting benchmark on %u cpus\n",
+	       online_cpus);
+
+	threads = (struct benchmark_thread *)
+		kmalloc(online_cpus * sizeof(struct benchmark_thread),
+			GFP_KERNEL);
+
+	for_each_online_cpu(cpu) {
+		init_completion(&threads[cpu].end);
+		threads[cpu].time = 0;
+	}
+
+	for_each_online_cpu(cpu) {
+		threads[cpu].prev = &main_start;
+		threads[cpu].thread = kthread_run(benchmark_thread,
+						  &threads[cpu],
+						  "lttng_benchmark");
+	}
+
+	complete_all(&main_start);
+
+	for_each_online_cpu(cpu) {
+		wait_for_completion_interruptible(&threads[cpu].end);
+	}
+	for_each_online_cpu(cpu) {
+		unsigned long long cpu_time = threads[cpu].time;
+		printk("lttng_benchmark: time for CPU#%u: %llu\n",
+		       cpu, cpu_time);
+		time += cpu_time;
+	}
+
+	kfree(threads);
+
+	printk("lttng_benchmark: total time: %llu\n", time);
 	return sprintf(buf, "%llu\n", time);
 }
 
